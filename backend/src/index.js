b45442675a7930de4b1d0e3a86dc80ef0558f45e
mrkeylost <nikosam08@gmail.com>
2026-04-21 21:58:52 +0700

    config deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import path from "path";
 import notesRouter from "./routes/notesRouter.js";
 import { connectDB } from "./repository/db.js";
 import AppError from "./utilities/appError.js";
@@ -8,21 +9,31 @@ import rateLimiter from "./middleware/rateLimiter.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 if (process.env.NODE_ENV !== "production") {
   dotenv.config({ path: "./.env" });
+
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
 }
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.use("*", (req, res, next) => {
   next(new AppError("Page Not Found", 404));
